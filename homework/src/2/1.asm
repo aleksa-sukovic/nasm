4057c76baa97bb90726d92441693948d8635cbc6
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2018-12-22 14:29:39 +0100

    Finishes homework 2 problem 1

--- homework/src/2/1.asm
+++ homework/src/2/1.asm
@@ -1,7 +1,15 @@
 %include "../../lib/asm_io.inc"
-
+;
+; Dat je tekst koji je definisan u okviru programa. Niz se završava null string terminator-
+; om. Tekst se sastoji od malih, velikih slova i interpunkcijskih znakova. Napisati program
+; na asemblerskom jeziku kojim se sva mala slova u tekstu konvertuju u velika, a sva velika
+; slova se konvertuju u mala. Ostale karakere ne treba menjati.
+;
+; Za ulaz: sOme Random STRING!.*tEsT@*#*$
+; Dobijamo izlaz: SoME rANDOM string!.*TeSt@*#*$
+;
 segment .data
-    text db 'sOme Random string', 0
+    text db 'sOme Random STRING!.*tEsT@*#*$', 0
 segment .bss
 
 segment .text
@@ -11,17 +19,45 @@ asm_main:
     enter 0,0
     pusha
         xor ebx, ebx ; offset
-        xor eax, eax
+        xor eax, eax ; current character
 
         transformation_loop:
             mov eax, [text + ebx]
             cmp eax, 0
             jz transformation_loop_end
-                call print_char
+                
+            ; char < 'A'
+            cmp al, 65
+            jl advance_loop
+
+            ; 'A' <= char <= 'Z
+            cmp al, 97
+            jl to_lower_case
+
+            ; 'a' <= char <= 'z'
+            cmp al, 122
+            jle to_upper_case
+
+            ; char > 'z'
+            jmp advance_loop
+
+            to_lower_case:
+                add al, 32
+                mov [text + ebx], al
+                jmp advance_loop
+            
+            to_upper_case:
+                sub al, 32
+                mov [text + ebx], al
+
+            advance_loop:
                 inc ebx
                 jmp transformation_loop
 
         transformation_loop_end:
+            ; printing transformed string
+            mov eax, text
+            call print_string
 
     popa
     mov eax, 0
